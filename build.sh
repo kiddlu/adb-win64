44fa7bf63a79e7a428c970ddb7c64bb1ed144688
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2017-02-13 17:16:48 +0800

    update to android-7.1.1_r4
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ cp $makefile android-adb/system/core/adb/makefile -f
 cd android-adb/system/core/adb/
 echo "\n>> Make... \n"
 make clean
-make
+cpus=$(grep '^processor' /proc/cpuinfo | wc -l) 
+make -j$(cpus)
 echo "\n>> Copying adb back into current dir...\n"
 cp adb.exe ../../../../
 echo "\n>> FINISH!\n"
--- get-sources.sh
+++ get-sources.sh
@@ -9,7 +9,8 @@ tar -xvzf openssl-1.0.2j.tar.gz
 # -------------------------
 
 # Branch to checkout from Android source code repo
-branch=android-7.0.0_r5
+branch=google/android-7.1.1_r4
+repo=ssh://172.16.0.9:29418/qualcomm
 
 # DOWNLOAD necessary files
 # -------------------------
@@ -19,21 +20,21 @@ mkdir android-adb
 cd android-adb
 mkdir system
 cd system
-git clone -b $branch https://android.googlesource.com/platform/system/core
-git clone -b $branch https://android.googlesource.com/platform/system/extras
+git clone -b $branch $repo/platform/system/core
+git clone -b $branch $repo/platform/system/extras
 cd ..
 mkdir external
 cd external
-git clone -b $branch https://android.googlesource.com/platform/external/zlib
-git clone -b $branch https://android.googlesource.com/platform/external/openssl
-git clone -b $branch https://android.googlesource.com/platform/external/libselinux
-git clone -b $branch https://android.googlesource.com/platform/external/zopfli/
-git clone -b $branch https://android.googlesource.com/platform/external/safe-iop/
+git clone -b $branch $repo/platform/external/zlib
+git clone -b $branch $repo/platform/external/openssl
+git clone -b $branch $repo/platform/external/libselinux
+git clone -b $branch $repo/platform/external/zopfli
+git clone -b $branch $repo/platform/external/safe-iop
 cd ..
 mkdir platform
 cd platform
-git clone -b $branch https://android.googlesource.com/platform/development/
-git clone -b $branch https://android.googlesource.com/platform/build/
+git clone -b $branch $repo/platform/development
+git clone -b $branch $repo/platform/build
 mkdir frameworks
 cd frameworks
-git clone -b $branch https://android.googlesource.com/platform/frameworks/base/
+git clone -b $branch $repo/platform/frameworks/base
